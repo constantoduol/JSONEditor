5cb1eb09df847f7884dc4edf1ef216087be075a5
Constant Oduol <constant@echomobile.org>
2017-08-16 16:59:24 +0300

    json editor initial design

--- src/JSONEditor.js
+++ src/JSONEditor.js
@@ -1,50 +1,84 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {isArray, isObject, isNumber, isString, isBoolean} from 'lodash';
+import {isArray, isObject, isNumber, isString, isBoolean, merge} from 'lodash';
 
 export default class JSONEditor extends React.Component{
 
-  static propTypes = {
-    data: PropTypes.object //the javascript object
-  };
+  static defaultProps = {
+    data: {},
+    marginLeftStep: 10,
+    marginBottom: 5
+  }
 
   parseData(){
     let {data} = this.props;
 
   }
 
-  renderText(){
-    return 
+  renderText(txt, marginLeft){
+    let {marginBottom} = this.props;
+    let style = merge({marginLeft, marginBottom}, styles.text);
+    return (
+      <input type="text" value={txt} style={style}/>
+    );
   }
 
-  renderNumber(){
-
+  renderNumber(num, marginLeft){
+    let {marginBottom} = this.props;
+    let style = merge({marginLeft, marginBottom}, styles.number);
+    return (
+      <input type="number" value={num} style={style}/>
+    );
   }
 
-  renderBoolean(){
-
+  renderBoolean(val, marginLeft){
+    let style = merge({marginLeft}, styles.boolean);
+    return (
+      <select style={style}>
+        <option value="true">True</option>
+        <option value="false">False</option>
+      </select>
+    );
   }
 
-  recursiveParseData(data){
+  renderLabel(label, marginLeft){
+    let style = merge({marginLeft}, styles.label);
+    return (
+      <label style={style}>{label}</label>
+    );
+  }
+
+
+  recursiveParseData(data, elems, marginLeft){
     if(isArray(data)){
       for(let val of data){
-        this.recursiveParseData(val);
+        this.recursiveParseData(val, elems, marginLeft);
       }
     } else if(isObject(data)){
       Object.keys(data).map(key => {
-        this.recursiveParseData(data[key]);
+        let isParent
+        elems.push(this.renderLabel(key, marginLeft + this.props.marginLeftStep));
+        elems.push(<br/>);
+        this.recursiveParseData(data[key], elems, marginLeft + this.props.marginLeftStep);
+        elems.push(<br/>);
       })
     } else if(isNumber(data)){
-      //number
-
+      elems.push(this.renderNumber(data, marginLeft));
     } else if(isString(data)) {
-      //text
+      elems.push(this.renderText(data, marginLeft));
     } else if(isBoolean(data)){
-      //bool
+      elems.push(this.renderBoolean(data, marginLeft));
     }
   }
 
   render(){
-    return <div>hello world</div>
+    let elems = [];
+    this.recursiveParseData(this.props.data, elems, 0);
+    return <div>{elems}</div>
   }
 }
+
+const styles = {
+  label: {
+    color: "#2196F3"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JSONEditor from './JSONEditor';
+import './index.css';
 
 ReactDOM.render(
-  <JSONEditor/>, 
+  <JSONEditor data={{
+    name: "constant", 
+    age: 20, 
+    city: "nairobi", 
+    work: "engineer", 
+    humans: {
+      name: "brad", 
+      age: 30
+    },
+    men: {
+      we: "are",
+      the: "men",
+      women: {
+        came: "from",
+        men: "also"
+      }
+    }
+  }}/>, 
   document.getElementById('root')
 );
