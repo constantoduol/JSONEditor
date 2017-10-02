4802778f280b111c512413955b82c2c12a9a4873
Constant Oduol <constant@echomobile.org>
2017-10-02 17:36:30 +0300

    use components

--- src/JSONEditor.js
+++ src/JSONEditor.js
@@ -1,85 +1,66 @@
 import React from 'react';
 import {isArray, isObject, isNumber, isString, isBoolean, merge} from 'lodash';
 
-export default class JSONEditor extends React.Component{
+export default class JSONEditor extends React.Component {
 
   static defaultProps = {
     data: {},
     marginLeftStep: 10,
-    marginBottom: 5
-  }
-
-  parseData(){
-    let {data} = this.props;
-
-  }
-
-  renderText(key, txt, marginLeft){
-    let {marginBottom} = this.props;
-    let style = merge({marginLeft, marginBottom}, styles.row);
-    return (
-      <div style={style}>
-        <div style={styles.label}>{key}</div>
-        <div style={styles.value}>
-          <input style={styles.input} type="text" value={txt}/>
-        </div>
-      </div>
-    );
-  }
-
-  renderNumber(key, num, marginLeft){
-    let {marginBottom} = this.props;
-    let style = merge({marginLeft, marginBottom}, styles.row);
-    return (
-      <div style={style}>
-        <div style={styles.label}>{key}</div>
-        <div style={styles.value}>
-          <input style={styles.input} type="number" value={num}/>
-        </div>
-      </div>
-    );
-  }
-
-  renderBoolean(key, val, marginLeft){
-    let style = merge({marginLeft}, styles.row);
-    return (
-      <div style={style}>
-        <div style={styles.label}>{key}</div>
-        <div style={styles.value}>
-          <select style={styles.select}>
-            <option value="true">True</option>
-            <option value="false">False</option>
-          </select>
-        </div>
-      </div>
-    );
-  }
-
-  renderLabel(label, marginLeft){
-    let style = merge({marginLeft}, styles.nestedLabel);
-    return (
-      <label style={style}>{label}</label>
-    );
+    marginBottom: 5,
+    collapsed: true
   }
 
 
   recursiveParseData(prevKey, data, elems, marginLeft){
     if(isArray(data)){
-      elems.push(this.renderLabel(prevKey, marginLeft));
+      elems.push(
+        <_Label 
+          value={prevKey} 
+          marginLeft={marginLeft}
+          hasChildren/>
+      );
+
       for(let val of data){
         this.recursiveParseData(val, elems, marginLeft);
       }
+
     } else if(isObject(data)){
-      elems.push(this.renderLabel(prevKey, marginLeft));
+
+      elems.push(
+        <_Label 
+          value={prevKey} 
+          marginLeft={marginLeft}
+          hasChildren/>
+      );
+
       Object.keys(data).map(key => {
         this.recursiveParseData(key, data[key], elems, marginLeft + this.props.marginLeftStep);
-      })
+      });
+
     } else if(isNumber(data)){
-      elems.push(this.renderNumber(prevKey, data, marginLeft));
+      elems.push(
+        <_Number 
+          marginLeft={marginLeft} 
+          marginBottom={this.props.marginBottom}
+          label={prevKey} 
+          value={data}/>
+      );
     } else if(isString(data)) {
-      elems.push(this.renderText(prevKey, data, marginLeft));
+      elems.push(
+        <_Text 
+          marginLeft={marginLeft} 
+          marginBottom={this.props.marginBottom}
+          label={prevKey} 
+          value={data}/>
+      );
     } else if(isBoolean(data)){
-      elems.push(this.renderBoolean(prevKey, data, marginLeft));
+      elems.push(
+        <_Boolean 
+          marginLeft={marginLeft} 
+          marginBottom={this.props.marginBottom}
+          label={prevKey} 
+          value={data}/>
+      );
     }
   }
 
@@ -90,9 +71,69 @@ export default class JSONEditor extends React.Component{
   }
 }
 
+const _Text = (props) => {
+  let {marginLeft, marginBottom, label, value} = props;
+  let style = merge({marginLeft, marginBottom}, styles.row);
+  return (
+    <div style={style}>
+      <_Label 
+        value={label} 
+        marginLeft={0}
+        hasChildren={false}/>
+      <div style={styles.value}>
+        <input style={styles.input} type="text" value={value}/>
+      </div>
+    </div>
+  )
+}
+
+const _Number = (props) => {
+  let {marginLeft, marginBottom, label, value} = props;
+  let style = merge({marginLeft, marginBottom}, styles.row);
+  return (
+    <div style={style}>
+      <_Label 
+        value={label} 
+        marginLeft={0}
+        hasChildren={false}/>
+      <div style={styles.value}>
+        <input style={styles.input} type="number" value={value}/>
+      </div>
+    </div>
+  )
+}
+
+const _Boolean = (props) => {
+  let {marginLeft, marginBottom, label, value} = props;
+  let style = merge({marginLeft, marginBottom}, styles.row);
+  return (
+    <div style={style}>
+      <_Label 
+        value={label} 
+        marginLeft={0}
+        hasChildren={false}/>
+      <div style={styles.value}>
+        <select style={styles.select} value={value}>
+          <option value="true">True</option>
+          <option value="false">False</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+const _Label = (props) => {
+  let {marginLeft, value, hasChildren} = props;
+  let style = hasChildren ? styles.withChildrenLabel : styles.label;
+  style = merge({marginLeft}, style);
+  return (
+    <div style={style}>{value}</div>
+  );
+}
+
 const styles = {
   label: {
-    color: "#2196F3",
+    color: "#00f",
     width: "50%"
   },
   value: {
@@ -105,7 +146,7 @@ const styles = {
   root: {
     margin: 5
   },
-  nestedLabel: {
+  withChildrenLabel: {
     color: "#a52a2a"
   },
   select: {
